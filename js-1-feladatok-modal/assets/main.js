071d108478e849008eaf26ab69d1937b0728dc6c
Nikol <nikolersek@gmail.com>
2021-11-03 14:07:29 +0100

    Modal is done.

---
+++ js-1-feladatok-modal/assets/main.js
@@ -0,0 +1,19 @@
+let modalIsVisible = false;
+const modalWrapper = document.querySelector(".overlay");
+const toggleModal = () => {
+    modalIsVisible = !modalIsVisible;
+    modalWrapper.style.opacity = modalIsVisible ? "1" : "0";
+}
+
+const buttons = document.querySelectorAll("button");
+for(let i = 0; i < buttons.length; i += 1) {
+    buttons[i].addEventListener("click", () => {
+        toggleModal();
+    });
+}
+
+modalWrapper.addEventListener("click", (event) => {
+    if(event.target.className == "overlay") {
+        toggleModal();
+    } 
+});
